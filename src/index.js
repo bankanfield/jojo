d96bd63dd1a96d1278c5aaadc8e18f11cc6daaa9
Jo <tanatip.jo@gmail.com>
2017-06-18 19:51:52 +0700

    add redux devtools config

--- src/index.js
+++ src/index.js
@@ -9,5 +9,16 @@ import { createStore } from "redux";
 import reducer from "./reducer/reducerindex.js";
 
 
-ReactDOM.render(<Provider store={createStore(reducer)}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider
+    store={
+      createStore(
+        reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+      }>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
